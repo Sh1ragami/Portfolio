c4c9efcfa859842dbaa41c3b8b8bce31c9f849ab
Sh1ragami <aoshi09275398@icloud.com>
2025-05-12 12:03:58 +0900

    Updates

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-// vite.config.js
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import glsl from "vite-plugin-glsl";
-
-export default defineConfig({
-  base: "/Portfolio/", // ← 👈 リポジトリ名と同じ大文字でOK（GitHub Pagesに合わせる）
-  plugins: [react(), glsl()],
-});
